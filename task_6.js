c7b4bc5bb903edcb7bdc055df9667a896b69ad66
NastyaQueenZhenya <queen@MacBook-Pro-Silkina.local>
2020-05-20 17:39:38 +0300

    Check it

---
+++ task_6.js
@@ -0,0 +1,17 @@
+/* Пусть теперь номер дня недели 
+хранится в переменной day, 
+например там лежит число 3. 
+Выведите день недели, 
+соответствующий значению переменной day */
+
+
+var week = {
+    First: 'Monday',
+    Second: 'Tuesday',
+    Third: 'Wednwsday',
+    Fourth: 'Thursday',
+    Fifth: 'Friday',
+    Sixth: 'Saturday',
+    Seventh: 'Sunday'
+}
+console.log(week.Third);
---
+++ tasks_5.js
@@ -0,0 +1,15 @@
+/* Создайте объект с днями недели. 
+Ключами в нем должны служить номера дней 
+от начала недели (понедельник - первый и т.д.). 
+Выведите на экран текущий день недели. */
+
+var week = {
+    First: 'Monday',
+    Second: 'Tuesday',
+    Third: 'Wednwsday',
+    Fourth: 'Thursday',
+    Fifth: 'Friday',
+    Sixth: 'Saturday',
+    Seventh: 'Sunday'
+}
+console.log(week.Third);
